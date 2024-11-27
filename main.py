950012e2ae2bdef06deb7ad889f4b7f639052d86
Seif Yasser <21p0102@eng.asu.edu.eg>
2024-11-27 02:24:00 +0200

    core: Wrapped Python scripts in Main.ipynb

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from src.utils import Utils, Monitor
 from src.preprocessing import Preprocessor
-# Any other imports you want to add can be added here
+
 
 if __name__ == "__main__":
     run_dir = "./run_dir"
     Utils.create_directories(run_dir=run_dir)
-    # Any other code you want to run when the main.py file is executed
 
     # ? Changing the preprocessor_config values will change the preprocessing steps
     '''
@@ -18,7 +17,7 @@ if __name__ == "__main__":
     Utils.preprocessor_config["Normalizer"] = "MinMaxScaler"
     Utils.preprocessor_config["Encoder"] = "LabelEncoder"
     dataset = Utils.load_data()
-    
+
     # Plotting the dataset
     Monitor.run(dataset)
 
@@ -27,5 +26,5 @@ if __name__ == "__main__":
     #! the final preprocessed dataset is returned with the following line
     dataset_preprocessed = preprocessor.preprocess()
 
-    print(dataset_preprocessed.head())
+    # print(dataset_preprocessed.head())
     print("main.py executed successfully")
